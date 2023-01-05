7b9a1804ccf015e5cb35acd8ef5e7fceb83bfdba
tom0407 <s3974735@rmit.edu.vn>
2023-01-05 15:29:04 +0700

    copy code

--- script.js
+++ script.js
@@ -8,3 +8,77 @@ $(document).ready(function(){
         }
     });
 });
+// openAim 
+const aimBtn = document.querySelector('.js-aim')
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-close-aim')
+
+function showAimDoc(){
+    modal.classList.add('open')
+}
+function hiddenAimDoc(){
+    modal.classList.remove('open')
+}
+
+aimBtn.addEventListener('click', showAimDoc)
+modalClose.addEventListener('click', hiddenAimDoc)
+
+// plan & progress
+const planBtn = document.querySelector('.js-plan')
+const modalPlan = document.querySelector('.js-modal-plan')
+const modalPlanClose = document.querySelector('.js-close-plan')
+
+function showPlanDoc(){
+    modalPlan.classList.add('open')
+}
+
+function hiddenPlanDoc(){
+    modalPlan.classList.remove('open')
+}
+
+planBtn.addEventListener('click', showPlanDoc)
+modalPlanClose.addEventListener('click', hiddenPlanDoc)
+
+// tool & technology
+const toolBtn = document.querySelector('.js-tool')
+const modalTool = document.querySelector('.js-modal-tool')
+const modalToolClose = document.querySelector('.js-close-tool')
+
+function showToolDoc(){
+    modalTool.classList.add('open')
+}
+function hiddenToolDoc(){
+    modalTool.classList.remove('open')
+}
+
+toolBtn.addEventListener('click', showToolDoc)
+modalToolClose.addEventListener('click', hiddenToolDoc)
+// testing
+const testingBtn = document.querySelector('.js-testing')
+const modalTesting = document.querySelector('.js-modal-testing')
+const modalTestingClose = document.querySelector('.js-close-testing')
+
+function showTestingDoc(){
+    modalTesting.classList.add('open')
+}
+function hiddenTestingDoc(){
+    modalTesting.classList.remove('open')
+}
+
+testingBtn.addEventListener('click',showTestingDoc)
+modalTestingClose.addEventListener('click',hiddenTestingDoc)
+
+// topic
+const topicBtn = document.querySelector('.js-topic')
+const modalTopic = document.querySelector('.js-modal-topic')
+const modalTopicClose = document.querySelector('.js-close-topic')
+
+function showTopicDoc(){
+    modalTopic.classList.add('open')
+}
+function hiddenTopicDoc(){
+    modalTopic.classList.remove('open')
+}
+
+topicBtn.addEventListener('click',showTopicDoc)
+modalTopicClose.addEventListener('click', hiddenTopicDoc)
